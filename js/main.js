15e3148ea316e78a5d0833d88fce3b310f69e741
Gavin Blair <gavin.blair@rtraction.com>
2014-03-10 17:33:11 -0400

    inventory, getting items

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ jQuery(document).ready(function($){
 		if(!glowaction) {
 			var target = e.offsetX;
 			//walk to position
-			walkto(target);
+			walkto(target, 40);
 		}
 		glowaction = false;
 	});
 	$('a').on('click touchup',function(){
 		//walk to the object
-		var pos = $(this).position();
-		walkto(pos.left, $(this).attr('id'));
+		var el = $(this);
+		var pos = el.position();
+		walkto(pos.left, el.width(), el.attr('id'));
 		return false;
 	});
 	$('#inventory div').click(function(){
@@ -45,8 +46,7 @@ jQuery(document).ready(function($){
 var pressing = false;
 var glowaction = false;
 var walking;
-var haskey = false;
-function walkto(target, id){
+function walkto(target, targetwidth, id){
 	if(walking) clearInterval(walking);
 	var sprite = $('#sprite');
 	sprite.addClass('walking');
@@ -66,7 +66,7 @@ function walkto(target, id){
 		var pos = sprite.position();
 		pos = pos.left;
 		sprite.css('left', pos+step);
-		if(pos < (target + 20) && pos > (target - 20)) {
+		if((pos < (target +targetwidth + 20) && pos > (target + targetwidth - 20)) || ((pos+52) < (target + 20) && (pos+52) > (target - 20))) {
 			sprite.removeClass('walking');
 			if(id !== undefined) {
 				arrivedat(id);
@@ -75,20 +75,55 @@ function walkto(target, id){
 		}
 	}, 100);
 }
+function hastool(tool){
+	return $('#inventory [data-tool='+tool+']').length;
+}
 function arrivedat(id){
+	var tool = $('#inventory .active').attr('data-tool');
 	switch(id){
 		case 'door':
-			if(!haskey){
-				say("It's locked. I wonder where the key is?");
-			} else {
-
+			if(tool === 'eye'){
+				if($('#door').hasClass('locked')){
+					say("It's a door.");
+				} else {
+					say("It's an open door. I wonder what's on the other side?");
+				}
+			} else if(tool === 'hand'){
+				if(hastool('key')) {
+					say("It's still locked. I wonder if this key will work?");
+				} else {
+					if($('#door').hasClass('locked')) {
+						say("It's locked. I wonder where the key is?");
+					} else {
+						say("you win!");
+					}
+				}
+			} else if(tool === 'key'){
+				$('#door').removeClass('locked');
+				$('#inventory [data-tool=key]').removeClass('has').attr('data-tool', '').text('');
+				$('#inventory [data-tool=eye]').click();
+				say("What was easy");
 			}
 		break;
 		case 'key':
-			say("It's a key. How convenient.");
+			if(tool === 'eye'){
+				say("It's a key. How convenient.");
+			} else if(tool === 'hand'){
+				$('#key').remove();
+				haskey = true;
+				$('#inventory .has:last').next().addClass('has').attr('data-tool', 'key').text('key');
+			}
 		break;
 	}
 }
 function say(text){
+	var t = $('#sprite span');
+	var pos = t.position();
+	pos = pos.left;
+	if(pos.left > ($('#game').width - t.width())){
+		t.css('left', 'auto').css('right', '0').css('text-align', 'right');
+	} else {
+		t.css('right', 'auto').css('left', '0').css('text-align', 'left');
+	}
 	$('#sprite span').text(text);
 }
